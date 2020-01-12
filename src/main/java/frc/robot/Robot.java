c423ffed5272f1840261c37ad813e75f498579aa
Michael Karpov <mkarpov@bsd220.org>
2020-01-12 14:22:18 -0600

    Worked on Command-based architecture

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -19,126 +22,28 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
 
   private Command testCommand;
-  private RobotContainer rContainer.
+  private RobotContainer rContainer;
+  private PowerDistributionPanel PDP = new PowerDistributionPanel(00);
 
   @Override
   public void robotInit() {
     PDP.clearStickyFaults();
-
-    // Set up input map
-    inputMap.put("leftX", 0);
-    inputMap.put("leftY", 1);
-    inputMap.put("rightX", 4);
-    inputMap.put("rightY", 5);
-
-    // Set up networktables
-    NetworkTableInstance tableInstance = NetworkTableInstance.getDefault();
-    NetworkTable table = tableInstance.getTable("datatable");
-    xOffsetData = table.getEntry("X");
-    System.out.println("\n---ROBOT INITIATED---\n\n Begin Log:\n\n\n");
+    System.out.println("Robot Initiated:");
+    rContainer = new RobotContainer();
   }
 
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-    System.out.println("\n---SETTING UP AUTONOMOUS---\n");
-    // START CODE HERE
-    m_timer.reset();
-    m_timer.start();
-    // END CODE HERE
-    System.out.println("\n---AUTONOMOUS INITIATED---\n");
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-
-    Number xOffset = xOffsetData.getNumber(0.0);
-
-    Double leftMotorValue = 0.0;
-    Double rightMotorValue = 0.0;
-    Double minThreshhold = 0.5;
-    Double MotorSpeedCap = 0.10;
-
-    leftMotorValue = xOffset.doubleValue() * MotorSpeedCap;
-    rightMotorValue = xOffset.doubleValue() * MotorSpeedCap;
-
-    if (Math.abs(leftMotorValue) < minThreshhold) {
-      leftMotorValue = 0.0;
-    }
-
-    if (Math.abs(rightMotorValue) < minThreshhold) {
-      rightMotorValue = 0.0;
-    }
-
-    // setMotors(leftMotorValue, rightMotorValue);
-    System.out.println(
-        "\n\n------\nxOffset: " + xOffset + "\nleftMotors: " + leftMotorValue + "\nrightMotors: " + rightMotorValue);
-  }
-
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
   @Override
   public void teleopInit() {
-    System.out.println("\n---SETTING UP TELEOP---\n");
-    // START CODE HERE
 
-    System.out.print("Enabling Talon Motor(s) and setting them to 0...\n");
-    setMotors(0.0, 0.0);
-    System.out.print("Done. \n");
-
-    m_timer.reset();
-    m_timer.start();
-    eStop = false;
-
-    // END CODE HERE
-    System.out.println("\n---TELEOP INITIATED---\n");
   }
 
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
   @Override
-  public void teleopPeriodic() {
-
-    if (driverControlJoystick.getRawButtonPressed(2) == true) {
-      System.out
-          .println("\n\n\nWARNING - Emergency stop button pressed. Disable and Re-enable teleop to enable driving.");
-      eStop = true;
-    }
-
-    if (eStop == false) {
-      // Dual Joystick system --
-      Double leftJoystickValue = driverControlJoystick.getRawAxis(inputMap.get("leftY"));
-      Double rightJoystickValue = driverControlJoystick.getRawAxis(inputMap.get("rightY"));
-
-      setMotors(leftJoystickValue * -0.5, rightJoystickValue * -0.5);
-      // both motors to be the joystick
-
-      // Print some logs
-      System.out.println("\nleftMotor: " + RoundNumber(leftJoystickValue * -100) + "%  ("
-          + leftMotorMain.getTemperature() + ")" + "\nrightMotor: " + RoundNumber(rightJoystickValue * -100) + "%  ("
-          + leftMotorMain.getTemperature() + ")");
-
-    } else {
-      setMotors(0.0, 0.0);
-    }
+  public void robotPeriodic() {
+    Scheduler.getInstance().run();
   }
-
-  /**
-   * This function is called periodically during test mode.
-   */
 
   @Override
   public void disabledInit() {
-    System.out.println("Disabling all motors...");
-    setMotors(0.0, 0.0);
-    System.out.println("Done. \n");
-
-    System.out.println("\n---ROBOT DISABLED---\n");
-    // Final Log
-    System.out.println("\nSummary:\nTotal Runtime: " + RoundNumber(m_timer.get()) + "s");
+    System.out.println("\n\n\n\n\n\n\n\n\nRobot Disabled");
   }
 }
